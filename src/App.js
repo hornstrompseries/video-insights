f8a084795777235f0d3f878defbd1fa22c2e21da
Rubén Cuadrado <cuadradosanchezruben@gmail.com>
2025-05-17 04:53:28 +0200

    🆕 Añadido filtro Recientes

--- src/App.js
+++ src/App.js
@@ -285,7 +285,7 @@ export default function App() {
               <tbody>
                 {keywords.map((k, i) => {
                   const icon =
-                    k.impacto > 50000000 ? "🚀" :
+                    k.impacto > 8000000 ? "🚀" :
                     k.impacto > 4000000 ? "📈" :
                     k.impacto > 200000 ? "➖" :
                     k.impacto > 100000 ? "📉" : "❌";
